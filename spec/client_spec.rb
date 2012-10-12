aa6ddf4c421ec5e98696f0ed56e1eaa3ba0db8d2
Miguel Guinada <mguinada@gmail.com>
2012-10-12 21:32:18 +0100

    Test environment is now isolated external agents
    * TCPServer mock
    * Mocked system process handling

--- spec/client_spec.rb
+++ spec/client_spec.rb
@@ -1,5 +1,7 @@
 describe VLC::Client do
   context 'initialization' do
+    before(:each) { mock_sub_systems }
+
     it 'starts a dedicated VLC instance by default' do
       vlc = VLC::Client.new
       vlc.should be_running
@@ -18,17 +20,17 @@ describe VLC::Client do
       vlc.should_not be_started
       vlc.should_not be_connected
     end
+  end
 
-    it 'is configurable' do
-      vlc = VLC::Client.new(:auto_start => false,
-                            :host => '192.168.1.10',
-                            :port => 9999,
-                            :headless => true)
+  it 'is configurable' do
+    vlc = VLC::Client.new(:auto_start => false,
+                          :host => '192.168.1.10',
+                          :port => 9999,
+                          :headless => true)
 
-      vlc.should_not be_running
-      vlc.host.should eq('192.168.1.10')
-      vlc.port.should eq(9999)
-      vlc.should be_headless
-    end
+    vlc.should_not be_running
+    vlc.host.should eq('192.168.1.10')
+    vlc.port.should eq(9999)
+    vlc.should be_headless
   end
 end
